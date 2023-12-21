e7ed71f13b959528835786fee499190a600df561
JessFreak <maliy.olexander@lll.kpi.ua>
2023-12-21 12:49:12 +0200

    fixed build

--- src/components/pages/main-page/Footer/Footer.tsx
+++ src/components/pages/main-page/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Box sx={styles.grid}>
         <Box>
           <Typography sx={styles.h1} variant='h1'>Stay In Touch</Typography>
-          <Typography sx={styles.body1} variant='body1'>Don't be a stranger and start following us :)</Typography>
+          <Typography sx={styles.body1} variant='body1'>Do not be a stranger and start following us :)</Typography>
         </Box>
         <Box sx={styles.icons}>
           <Box sx={styles.icon} style={{ backgroundImage: 'url(icons/Footer/facebook.png)' }}></Box>
