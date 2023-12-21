e38dc4b705e090b4c320cd35258481a68c538628
JessFreak <maliy.olexander@lll.kpi.ua>
2023-12-21 11:15:03 +0200

    finish footer part

--- src/components/pages/main-page/Footer/Footer.style.ts
+++ src/components/pages/main-page/Footer/Footer.style.ts
@@ -3,20 +3,52 @@ import { SxProps } from '@mui/system';
 export const mainContainer: SxProps = {
   width: '100vw',
   height: '30vh',
+  display: 'grid',
+  justifyItems: 'center',
+  alignItems: 'center',
 };
 
 export const grid: SxProps = {
+  width: '100%',
   display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  justifyItems: 'center',
+  alignItems: 'center',
 };
 
-export const text: SxProps = {
+export const h1: SxProps = {
+  fontFamily: 'Yeseva One',
+  fontSize: '38px',
+  fontWeight: 'bold',
+  lineHeight: 'normal',
+};
 
+export const body1: SxProps = {
+  fontFamily: 'DM Sans',
+  fontSize: '18px',
+  fontWeight: 400,
+  lineHeight: '34px',
+};
+
+export const icons: SxProps = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(4, 1fr)',
+  columnGap: '16px',
+};
+
+export const icon: SxProps = {
+  width: '53px',
+  height: '53px',
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: 'lightpink',
+  },
 };
 
 export const bg: SxProps = {
   backgroundImage: 'url(/icons/Footer/bg.png)',
   position: 'relative',
-  top: '50%',
+  bottom: '-1rem',
   width: '100%',
   height: '164px',
 };
--- src/components/pages/main-page/Footer/Footer.tsx
+++ src/components/pages/main-page/Footer/Footer.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import * as styles from './Footer.style';
 
 const Footer = () => {
   return (
     <Box sx={styles.mainContainer}>
       <Box sx={styles.grid}>
-        <Box>text</Box>
-        <Box>icons</Box>
+        <Box>
+          <Typography sx={styles.h1} variant='h1'>Stay In Touch</Typography>
+          <Typography sx={styles.body1} variant='body1'>Don't be a stranger and start following us :)</Typography>
+        </Box>
+        <Box sx={styles.icons}>
+          <Box sx={styles.icon} style={{ backgroundImage: 'url(icons/Footer/facebook.png)' }}></Box>
+          <Box sx={styles.icon} style={{ backgroundImage: 'url(icons/Footer/instagram.png)' }}></Box>
+          <Box sx={styles.icon} style={{ backgroundImage: 'url(icons/Footer/discord.png)' }}></Box>
+          <Box sx={styles.icon} style={{ backgroundImage: 'url(icons/Footer/dribbble.png)' }}></Box>
+        </Box>
       </Box>
       <Box sx={styles.bg}></Box>
     </Box>
